98cdfeb2db7f839565b033fa841c57f6092fccc8
Palak <palakkumar1998@gmail.com>
2023-10-23 21:58:45 +0530

    neccesary comments

--- Context/authContext.js
+++ Context/authContext.js
@@ -1,8 +1,12 @@
 import {createContext, useContext, useState } from "react";
 
-const UserContext = createContext({children});
+// create a context using createContext hook and pass a parameter
+const UserContext = createContext({children}); 
 
+// export a function which return a component with some values where thier initiale value is null and true 
 export const UserProvider = () => {
+
+    // create two states using usestate hook 
     const [currentUser, setcurrentUser] = useState(null);
     const [isLoading, setisLoading] = useState(true);
     return (
@@ -12,5 +16,5 @@ export const UserProvider = () => {
     )
 
 }
-
+// export another  function which take another fuction (callback) 
 export const useAuth = () => useContext(UserContext);

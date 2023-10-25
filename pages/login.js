4b64014c11e222ac94fa69ced135cecc0a387b58
Palak <palakkumar1998@gmail.com>
2023-10-25 18:19:41 +0530

    change in text, bg colors

--- pages/login.js
+++ pages/login.js
@@ -25,24 +25,24 @@ const Login = () => {
 
 
   return (
-    <div className="h-[100vh] flex justify-center items-center bg-c1">
+    <div className="h-[100vh] flex justify-center items-center bg-white">
       <div className="flex items-center flex-col">
 
         <div className="text-center">
-          <div className="text-4xl font-bold"> {currentUser} to your Account</div>
+          <div className="text-2xl font-bold text-c1"> {currentUser} to your Account</div>
           <div className="mt-3 text-c3">Connect and chat with anyone, anywhere</div>
         </div>
 
         <div className="flex items-center gap-2 w-full mt-10 mb-5 ">
           {/* // login button */}
-          <div className="bg-gradient-to-r from-indigo-500 via-sky-500  to-emerald-500 w-1/2 h-14 rounded-md cursor-pointer p-[1px]">
-            <div className="flex items-center justify-center gap-3 text-white bg-c1 w-full h-full rounded-md font-semibold" >
+          <div className=" w-1/2 h-14 rounded-md cursor-pointer p-[1px]">
+            <div className="flex items-center justify-center gap-3 text-white bg-orange-500 hover:bg-orange-600 w-full h-full rounded-md font-semibold" >
               <IoLogoGoogle size={24} />
               <span>Login with Google</span>
             </div>
           </div>
-          <div className="bg-gradient-to-r from-indigo-500 via-sky-500  to-emerald-500 w-1/2 h-14 rounded-md cursor-pointer p-[1px]">
-            <div className="flex items-center justify-center gap-3 text-white bg-c1 w-full h-full rounded-md font-semibold" >
+          <div className=" w-1/2 h-14 rounded-md cursor-pointer p-[1px]">
+            <div className="flex items-center justify-center gap-3 text-white bg-blue-400 hover:bg-blue-600 w-full h-full rounded-md font-semibold" >
               <IoLogoFacebook size={24} />
               <span>Login with Facebook</span>
             </div>
@@ -57,9 +57,9 @@ const Login = () => {
         {/* login form  */}
         <form onSubmit={handleSubmit} className='flex flex-col items-center gap-3 mt-5  w-[500px]'>
           <input type="email"
-            placeholder='enter your email here' className='w-full h-14 bg-c5 rounded-xl outline-none border-none px-5 text-c3 ' autoComplete='off' />
+            placeholder='enter your email here' className='w-full h-14 bg-slate-300 rounded-xl outline-none border-none px-5 text-c3 ' autoComplete='off' />
           <input type="password"
-            placeholder='enter your password here' className='w-full h-14 bg-c5 rounded-xl outline-none border-none px-5 text-c3 ' autoComplete='off' />
+            placeholder='enter your password here' className='w-full h-14 bg-slate-300 rounded-xl outline-none border-none px-5 text-c3 ' autoComplete='off' />
           <div className="text-right w-full text-c3">
             <span className='cursor-pointer'> Forget Password ?</span>
           </div>
@@ -68,7 +68,7 @@ const Login = () => {
         {/* registration link */}
         <div className="flex justify-center gap-1 text-c3 mt-5">
           <span>Not a member yet ?</span>
-          <Link href="/register" className='font-semibold text-white underline underline-offset-3 cursor-pointer'> Register Now </Link>
+          <Link href="/register" className='font-semibold text-c2 underline underline-offset-3 cursor-pointer'> Register Now </Link>
         </div>
 
       </div>

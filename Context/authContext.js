ef527b6c6143e6883716dbdec9ce2bd6424d18f5
Palak <palakkumar1998@gmail.com>
2023-10-26 23:24:47 +0530

    log out , sign out set up completed

--- Context/authContext.js
+++ Context/authContext.js
@@ -1,6 +1,7 @@
 import { auth } from "@/Firebase/firebase";
-import {onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut as authSignOut } from "firebase/auth";
 import { createContext, useContext, useState, useEffect } from "react";
+
 
 
 // create a context using createContext hook 
@@ -8,16 +9,10 @@ const UserContext = createContext();
 
 // export a function which return a component with some values where thier initiale value is null and true 
 export const UserProvider = ({ children }) => {
-// create two states using usestate hook 
-const [currentUser, setcurrentUser] = useState("Palak Kumar");
-const [isLoading, setisLoading] = useState(true);
-
-// to clear the loading and currentuser
-    const clear = () => {
-        setcurrentUser(null);
-        setisLoading(false);
-    }
-
+    // create two states using usestate hook 
+    const [currentUser, setcurrentUser] = useState("Palak Kumar");
+    const [isLoading, setisLoading] = useState(true);
+    // METHOD 1 : to change the auth state & check user is exist or not
     const authStateChanged = (user) => {
         setisLoading(true);
 
@@ -29,14 +24,30 @@ const [isLoading, setisLoading] = useState(true);
         setisLoading(false);
     }
 
+    // METHOD 2: to clear the loading and currentuser
+    const clear = () => {
+        setcurrentUser(null);
+        setisLoading(false);
+    }
+
+
+    // METHOD 3: 
+    const signOut = () => {
+        authSignOut(auth).then(() =>
+            clear()
+        )
+    }
+
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, authStateChanged);
         return () => unsubscribe();
     }, []);
 
 
+
+
     return (
-        <UserContext.Provider value={{ currentUser, setcurrentUser, isLoading, setisLoading }}>
+        <UserContext.Provider value={{ currentUser, setcurrentUser, isLoading, setisLoading, signOut }}>
             {children}
         </UserContext.Provider>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
-// import React from 'react'
+import { useAuth } from '@/Context/authContext'
+import { useRouter } from 'next/router'
+import { React, useEffect } from 'react'
+
+
 
 const Home = () => {
+  const { signOut, currentUser, isLoading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && !currentUser) {
+      router.push("/login")
+    }
+
+  }, [currentUser, isLoading])
+
   return (
-    <div>Home Page</div>
+    <div className='text-c1'>
+      <button onClick={signOut} >  sign out</button>
+
+    </div>
+
   )
 }
 

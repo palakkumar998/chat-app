822949e5b52c72d46e118130528de93bdc3919bc
Palak <palakkumar1998@gmail.com>
2023-11-17 23:23:44 +0530

    chatId creation

--- Context/ChatContext.js
+++ Context/ChatContext.js
@@ -1,11 +1,38 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
+import { useAuth } from "./authContext";
+
 
 const chatContext = createContext();
 
+
 export const ChatContextProvider = ({ children }) => {
     const [users, setUsers] = useState(false);
+    const { currentUser } = useAuth();
+
+    const INITAIL_STATE = {
+
+        chatId: "",
+        user: null
+    }
+
+    const chatReducer = (state, action) => {
+        switch (action.type) {
+            case "CHANGE_USER":
+                return {
+                    user: action.payload,
+                    chatId: currentUser.uid > action.payload.uid ? currentUser.uid + action.payload.uid : action.payload.uid + currentUser.uid
+                }
+
+            default:
+                return state;
+        }
+
+    }
+
+    const [state, dispatch] = useReducer(chatReducer, INITAIL_STATE);
+
     return (
-        <chatContext.Provider value={{ users, setUsers }}>
+        <chatContext.Provider value={{ users, setUsers, data: state, dispatch }}>
             {children}
         </chatContext.Provider >
     )

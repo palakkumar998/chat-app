d2e34d616c073a493eb4df92e0f470f1c4093281
Palak <palakkumar1998@gmail.com>
2023-11-18 22:10:58 +0530

    chat collection between 2/more users

--- Components/popups/UserPopup.jsx
+++ Components/popups/UserPopup.jsx
@@ -3,11 +3,78 @@ import PopupWrapper from './PopupWrapper'
 import { useAuth } from '@/Context/authContext'
 import { userChatContext } from '@/Context/ChatContext'
 import Avatar from '../Avatar'
+import {
+	doc,
+	getDoc,
+	serverTimestamp,
+	setDoc,
+	updateDoc,
+} from 'firebase/firestore'
+import { db } from '@/Firebase/firebase'
 // import { db } from '@/Firebase/firebase'
 
-const UserPopup = (props) => {
+const UserPopup = (
+	props) => {
 	const { currentUser } = useAuth()
-	const { users } = userChatContext()
+	const { users, dispatch } = userChatContext()
+
+	const handleSelect = async (user) => {
+		try {
+			const combinedId =
+				currentUser.uid > user.uid
+					? currentUser.uid + user.uid
+					: user.uid + currentUser.uid
+
+			const res = await getDoc(doc(db, 'chats', combinedId))
+
+			if (!res.exists()) {
+				// chat document doesn't exists
+				await setDoc(doc(db, 'chats', combinedId), {
+					//? it will create a doc in DB, collection name : chats, with message array (empty) where chat is combine id
+					messages: [],
+				})
+
+				const currentUserChatRef = await getDoc(
+					doc(db, 'userChats', currentUser.uid) //? taking refrence of userchats (Collection) with current user uid
+				)
+
+				const userChatRef = await getDoc(doc(db, 'userChats', user.uid)) //? taking refrence of userchats (Collection) with user uid
+
+				//? Checking user is exist or not
+				if (!currentUserChatRef.exists()) {
+					await setDoc(doc(db, 'userChats', currentUser.uid), {})
+				}
+
+				await updateDoc(doc(db, 'userChats', currentUser.uid), {
+					[combinedId + '.userInfo']: {
+						uid: user.uid,
+						displayName: user.displayName,
+						photoURL: user.photoURL || null,
+						color: user.color,
+					},
+					[combinedId + '.date']: serverTimestamp(),
+				})
+				if (!userChatRef.exists()) {
+					await setDoc(doc(db, 'userChats', user.uid), {})
+				}
+				await updateDoc(doc(db, 'userChats', user.uid), {
+					[combinedId + '.userInfo']: {
+						uid: currentUser.uid,
+						displayName: currentUser.displayName,
+						photoURL: currentUser.photoURL || null,
+						color: currentUser.color,
+					},
+					[combinedId + '.date']: serverTimestamp(),
+				})
+			} else {
+				// chat document  exists
+			}
+			dispatch({ type: 'CHANGE_USER', payload: user })
+			props.onHide()
+		} catch (error) {
+			console.error(error)
+		}
+	}
 	return (
 		<PopupWrapper {...props}>
 			<div className="mt-5 flex flex-col gap-2 grow relative overflow-auto scrollbar ">
@@ -15,8 +82,10 @@ const UserPopup = (props) => {
 					{users &&
 						Object.values(users).map((user) => (
 							// eslint-disable-next-line react/jsx-key
-							<div className="flex items-center gap-4 rounded-xl hover:bg-c5 py-2 px-4 cursor-pointer">
-								
+							<div
+								className="flex items-center gap-4 rounded-xl hover:bg-c5 py-2 px-4 cursor-pointer"
+								onClick={() => handleSelect(user)}
+							>
 								<Avatar size="large" user={user} />
 
 								<div className="flex flex-col gap-1 grow">

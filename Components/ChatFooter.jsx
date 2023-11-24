b73810898cf23c32170b85478a3246708a5e7b52
Palak <palakkumar1998@gmail.com>
2023-11-24 20:11:10 +0530

    emoji picker structure done

--- Components/ChatFooter.jsx
+++ Components/ChatFooter.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Icon from './Icon'
 import { CgAttachment } from 'react-icons/cg'
 import { HiOutlineEmojiHappy } from 'react-icons/hi'
 import Composebar from './Composebar'
+import EmojiPicker from 'emoji-picker-react'
+import ClickAwayListener from 'react-click-away-listener'
+
 const ChatFooter = () => {
+	const onEmojiClick = () => {}
+	const [showImojiPicker, setshowImojiPicker] = useState(false)
 	return (
 		<div className="flex items-center p-2 rounded-xl bg-c1/[0.8] relative">
 			<div className="shrink-0">
@@ -29,10 +34,26 @@ const ChatFooter = () => {
 						<HiOutlineEmojiHappy
 							size={24}
 							className="text-blue-300"
+							onClick={() => setshowImojiPicker(true)}
 						/>
 					}
 				/>
+				{showImojiPicker && (
+					<ClickAwayListener
+						onClickAway={() => setshowImojiPicker(false)}
+					>
+						<div className="absolute bottom-14 left-2 shadow-2xl">
+							<EmojiPicker
+								theme="light"
+								emojiStyle="native"
+								onEmojiClick={onEmojiClick}
+								autoFocusSearch={false}
+							/>
+						</div>
+					</ClickAwayListener>
+				)}
 			</div>
+
 			<Composebar />
 		</div>
 	)

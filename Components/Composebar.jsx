a5fbd101b8516108f47e0d53b0ac5fcf44b19183
Palak <palakkumar1998@gmail.com>
2023-11-25 19:16:58 +0530

    input text structure & logic

--- Components/Composebar.jsx
+++ Components/Composebar.jsx
@@ -1,7 +1,33 @@
+import { userChatContext } from '@/Context/ChatContext'
 import React from 'react'
+import { TbSend } from 'react-icons/tb'
 
 const Composebar = () => {
-	return <div>Composebar</div>
+	const { inputText, setInputText } = userChatContext()
+	const handleTyping = (e) => {
+		setInputText(e.target.value)
+	}
+	const onKeyUp = () => {}
+
+	return (
+		<div className="flex items-center gap-2 grow">
+			<input
+				type="text"
+				className="grow w-full outline-0 px-2 py-2 text-white bg-transparent placeholder:text-c3 outline-none text-base "
+				value={inputText}
+				placeholder="Type a message"
+				onChange={handleTyping}
+				onKeyUp={onKeyUp}
+			/>
+			<button
+				className={`h-10 w-10 rounded-full shrink-0 flex justify-center items-center ${
+					inputText.trim().length > 0 ? 'bg-green-500' : ''
+				} `}
+			>
+				<TbSend className="text-white" size={20} />
+			</button>
+		</div>
+	)
 }
 
 export default Composebar

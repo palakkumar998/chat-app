592e8ed9a543cddc6acb103a9b7f87d9eb0a5b3e
Palak <palakkumar1998@gmail.com>
2023-12-08 20:19:17 +0530

    user blocking & unblocking logic done

--- Components/Chat.jsx
+++ Components/Chat.jsx
@@ -3,14 +3,41 @@ import ChatHeader from './ChatHeader'
 import Messages from './Messages'
 import { userChatContext } from '@/Context/ChatContext'
 import ChatFooter from './ChatFooter'
+import { useAuth } from '@/Context/authContext'
 
 const Chat = () => {
-	const { data } = userChatContext()
+	const { currentUser } = useAuth()
+	const { data, users } = userChatContext()
+
+	const handleClickAway = () => {
+		setshowMenu(false)
+	}
+	
+	const isUserBlocked = users[currentUser.uid]?.blockedUsers?.find(
+		(u) => u === data.user.uid
+	)
+	const iAmBlocked = users[data.user.uid]?.blockedUsers?.find(
+		(u) => u === currentUser.uid
+	)
+
+
 	return (
 		<div className="flex flex-col grow p-5 ">
 			<ChatHeader />
 			{data.chatId && <Messages />}
-			<ChatFooter />
+			{!isUserBlocked && !iAmBlocked && <ChatFooter />}
+			{/*//?------->/  CONDITION FOR SHOWING MSG WHEN YOU BLOCK THE USER /<--------------  */}
+			{isUserBlocked && (
+				<div className="w-full text-center text-c3 py-5">
+					This user has been blocked
+				</div>
+			)}
+			{/*//?------->/  CONDITION FOR SHOWING MSG WHEN SOMEONE BLOCK THE YOU /<--------------  */}
+			{iAmBlocked && (
+				<div className="w-full text-center text-c3 py-5">
+					{`${data.user.displayName} has Blocked you`}
+				</div>
+			)}
 		</div>
 	)
 }
--- Components/ChatMenu.jsx
+++ Components/ChatMenu.jsx
@@ -1,17 +1,57 @@
+import { userChatContext } from '@/Context/ChatContext'
+import { useAuth } from '@/Context/authContext'
+import { db } from '@/Firebase/firebase'
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore'
 import React from 'react'
 import ClickAwayListener from 'react-click-away-listener'
 
 const ChatMenu = ({ showMenu, setshowMenu }) => {
+	const { currentUser } = useAuth()
+	const { data, users } = userChatContext()
+
 	const handleClickAway = () => {
-		setshowMenu(false);
+		setshowMenu(false)
+	}
+
+
+	const isUserBlocked = users[currentUser.uid]?.blockedUsers?.find(
+		(u) => u === data.user.uid
+	)
+	const iAmBlocked = users[data.user.uid]?.blockedUsers?.find(
+		(u) => u === currentUser.uid
+	)
+
+	
+	const handleBlock = async (action) => {
+		// ?------->/ TO BLOCK USER /<----------------/
+		if (action === 'block') {
+			await updateDoc(doc(db, 'users', currentUser.uid), {
+				blockedUsers: arrayUnion(data.user.uid),
+			})
+		}
+
+		//?-------->/ TO UNBLOCK USER /<--------------/
+		if (action === 'unblock') {
+			await updateDoc(doc(db, 'users', currentUser.uid), {
+				blockedUsers: arrayRemove(data.user.uid),
+			})
+		}
 	}
 	return (
 		<ClickAwayListener onClickAway={handleClickAway}>
 			<div className="w-[200px] absolute top-[70px] right-5 bg-c0 z-10 rounded-md overflow-hidden ">
 				<ul className="flex flex-col py-2">
-					<li className="flex py-3 px-5 items-center hover:bg-black cursor-pointer">
-						Block User
-					</li>
+					{!iAmBlocked && (
+						<li
+							className="flex py-3 px-5 items-center hover:bg-black cursor-pointer"
+							onClick={(e) => {
+								e.stopPropagation()
+								handleBlock(isUserBlocked ? 'unblock' : 'block')
+							}}
+						>
+							{isUserBlocked ? 'Unblock' : 'Block'}
+						</li>
+					)}
 					<li className="flex py-3 px-5 items-center hover:bg-black cursor-pointer">
 						Share Profile
 					</li>
